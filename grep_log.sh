ca02dbf4fa8cd5b3699e0fbbc0c01d05ce2174c1
Christian Huber <ukekk@student.kit.edu>
2023-12-21 14:54:47 +0100

    Better scoring file structure

---
+++ grep_log.sh
@@ -0,0 +1,4 @@
+
+clear
+cat $1 | grep eval_loss | cut -d" " -f2 | sed "s/,//g" | nl | sort -n -k2,2 | head -n5 && echo "" && cat $1 | grep eval_loss | cut -d" " -f2 | sed "s/,//g" | nl | tail -n1
+
